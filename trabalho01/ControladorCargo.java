e618a5bbad106cb553fea3c33429e3529c86c81e
guiviero <32081233+guiviero@users.noreply.github.com>
2017-09-25 17:20:48 -0300

    Add files via upload

---
+++ trabalho01/ControladorCargo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho01;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class ControladorCargo {
+    private ArrayList<Cargo> cargos;
+    
+    public void inserirCargo(Cargo cargo) {
+        
+    }
+    
+    public void alterarNivelCargoPeloCodigo (int codigoCargo) {
+        
+    }
+    
+    public void buscarCargoPeloCodigo (int codigoCargo) {
+        
+    }
+    
+    public void deletarCargoPeloCodigo (int codigoCargo) {
+        
+    }
+    
+    public ArrayList<Cargo> listarCargos() {
+        return this.cargos;
+    } 
+}
---
+++ trabalho01/ControladorFuncionario.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho01;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class ControladorFuncionario {
+    
+    private ArrayList<Funcionario> funcionarios;
+
+    public ControladorFuncionario() {
+        
+    }
+    
+    public void inserirFuncionario(Funcionario funcionario) {
+        
+    }
+    
+    public void alterarNivelFuncionarioPeloCodigo (int codigoFuncionario) {
+        
+    }
+    
+    public void buscarFuncionarioPeloCodigo (int codigoFuncionario) {
+        
+    }
+    
+    public void deletarFuncionarioPeloCodigo (int codigoFuncionario) {
+        
+    }
+    
+    public ArrayList<Funcionario> listarFuncionarios() {
+        return this.funcionarios;
+    } 
+    
+}
